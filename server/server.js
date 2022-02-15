7caf25e266c3443cfbcde82118e514d9931c0b21
Ramunas Zybartas <ramunzzz007@yahoo.es>
2022-02-15 08:05:30 +0200

    feature/authentication

--- server/server.js
+++ server/server.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
+
+const { port, dbConfig } = require('./config');
+const auth = require('./src/routes/v1/auth');
 
 
-const app = express();
+const main = async () => {
+    const app = express();
 
-app.use(express.json());
-app.use(cors());
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+
+        app.use(express.json());
+        app.use(cors());
+
+        app.mysql = connection;
+
+        app.use('/v1/auth', auth);
+
+        app.get('/', (req, res) => {
+            res.send('ok')
+        })
+
+        app.get('*', (req, res) => {
+            res.status(404).send({error: 'Page not found'})
+        })
+
+        app.listen(port, () => {
+            console.log(`Server running on port: ${port}`);
+        })
+    } catch (error) {
+        console.error(error, 'Something wrong with database');
+    }
+};
+
+main();
